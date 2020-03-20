0d3602d80155fed5b341fb39bc4920c59428187d
github username <github email address>
2020-03-19 21:40:31 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,53 @@
-
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: []
+}, action) {
+  console.log(state.restaurants)
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+    console.log('Adding', action.text)
 
+    const restaurant = {
+      id: cuidFn(),
+      text: action.text
+    }
+
+    return {
+      ...state, restaurants: [...state.restaurants, restaurant]
+    }
+
+    case 'DELETE_RESTAURANT':
+      console.log('Deleting...', action.id)
+
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return {
+        ...state, restaurants
+      }
+
+    case 'ADD_REVIEW':
+    console.log('Adding review...', action.review.text)
+
+    const review = {
+      id: cuidFn(),
+      restaurantId: action.review.restaurantId,
+      text: action.review.text
+    };
+
+    return {
+      ...state, reviews: [...state.reviews, review]
+    }
+
+    case 'DELETE_REVIEW':
+    console.log('Deleting review...')
+    const reviews = state.reviews.filter(review => review.id !== action.id)
+    return {
+      ...state, reviews
+    }
+
+    default:
+      return state;
+  }
 }
