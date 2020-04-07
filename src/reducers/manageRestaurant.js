7b779980480aac9192e8de5a6b5db247a55c1d5a
cejimenez0123 <christianjimenez0123@gmail.com>
2020-04-07 12:59:59 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,23 @@
 
 import cuid from 'cuid';
+import Restaurants from '../components/restaurants/Restaurants';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default (state = {
+restaurants: []}
+, action) =>{
+    switch(action.type){
+        case "ADD_RESTAURANT":
+            let restaurant = {text: action.payload, id: cuid()}  
+            return {...state,restaurants:[...state.restaurants, restaurant]}
+        case "DELETE_RESTAURANT":
+            let x = state.restaurants.filter(u =>{
+                
+                u.id !== action.payload})
+                console.log("hit",x)
+                debugger
+            return {...state, restaurants:[]}
+        default:
+            return state
+    }
 }
