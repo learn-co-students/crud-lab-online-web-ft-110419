021fe1dc5a7f24ea231ac471682d62181cd57d85
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-13 19:42:45 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
-    return(
+    return (
       <ul>
-        Restaurants Component
+        {this.props.restaurants.map(r => (
+          <Restaurant restaurant={r} key={r.id} deleteRestaurant={this.props.deleteRestaurant} />
+        ))}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,28 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = { restaurants: [], reviews: [] }, action) {
+  switch (action.type) {
+    case "ADD_RESTAURANT":
+      console.log(action)
+      const newRestaurant = {
+        id: cuidFn(),
+        text: action.text
+      }
+      return { ...state, restaurants: [...state.restaurants, newRestaurant] }
+    case "DELETE_RESTAURANT":
+      return { ...state, restaurants: state.restaurants.filter(r => r.id !== action.payload) }
+    case "ADD_REVIEW":
+      const newReview = {
+        restaurantId: action.review.restaurantId,
+        text: action.review.text,
+        id: cuidFn()
+      }
 
+      return { ...state, reviews: [...state.reviews, newReview] }
+    case "DELETE_REVIEW":
+      return { ...state, reviews: state.reviews.filter(r => r.id !== action.id) }
+    default:
+      return state;
+  }
 }
